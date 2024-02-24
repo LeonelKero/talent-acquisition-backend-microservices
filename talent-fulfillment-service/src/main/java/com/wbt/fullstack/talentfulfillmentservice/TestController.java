cbde471690cba7995443bf9fb704a57c8aead74e
leonel ka <waboleonel@gmail.com>
2024-02-24 21:22:33 +0100

    Code refactoring paths

--- talent-fulfillment-service/src/main/java/com/wbt/fullstack/talentfulfillmentservice/TestController.java
+++ talent-fulfillment-service/src/main/java/com/wbt/fullstack/talentfulfillmentservice/TestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = {"/careers"})
+@RequestMapping(path = {"/fulfill"})
 public class TestController {
 
     private final Environment env;
--- talent-request-service/src/main/java/com/wbt/fullstack/talentrequestservice/TestController.java
+++ talent-request-service/src/main/java/com/wbt/fullstack/talentrequestservice/TestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = {"/test"})
+@RequestMapping(path = {"/request"})
 public class TestController {
 
     @Value("${spring.application.name}")
