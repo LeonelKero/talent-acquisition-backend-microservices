c8338bdf02eb5f713f3e3607df5ecfa8ca8ff1dd
leonel ka <waboleonel@gmail.com>
2024-02-24 05:11:29 +0100

    Configure eureka discovery server

--- talent-discovery-service/src/main/java/com/wbt/fullstack/talentdiscoveryservice/TalentDiscoveryServiceApplication.java
+++ talent-discovery-service/src/main/java/com/wbt/fullstack/talentdiscoveryservice/TalentDiscoveryServiceApplication.java
@@ -2,8 +2,10 @@ package com.wbt.fullstack.talentdiscoveryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class TalentDiscoveryServiceApplication {
 
 	public static void main(String[] args) {
