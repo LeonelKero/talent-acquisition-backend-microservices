29e26b1cbd720a87062666b6feb626f5e4e345f6
leonel ka <waboleonel@gmail.com>
2024-02-24 05:25:34 +0100

    Configure eureka client and server ports

--- career-portal-service/src/main/java/com/wbt/fullstack/careerportalservice/CareerPortalServiceApplication.java
+++ career-portal-service/src/main/java/com/wbt/fullstack/careerportalservice/CareerPortalServiceApplication.java
@@ -2,8 +2,10 @@ package com.wbt.fullstack.careerportalservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CareerPortalServiceApplication {
 
 	public static void main(String[] args) {
--- talent-request-service/src/main/java/com/wbt/fullstack/talentrequestservice/TalentRequestServiceApplication.java
+++ talent-request-service/src/main/java/com/wbt/fullstack/talentrequestservice/TalentRequestServiceApplication.java
@@ -2,9 +2,10 @@ package com.wbt.fullstack.talentrequestservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-
+@EnableDiscoveryClient
 public class TalentRequestServiceApplication {
 
 	public static void main(String[] args) {
