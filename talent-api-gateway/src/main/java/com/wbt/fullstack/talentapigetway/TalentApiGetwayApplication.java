9b31eba9fa821f737d0cd7d27173abebd528163f
leonel ka <waboleonel@gmail.com>
2024-02-24 05:46:53 +0100

    Configure api gateway

--- talent-api-gateway/src/main/java/com/wbt/fullstack/talentapigetway/TalentApiGetwayApplication.java
+++ talent-api-gateway/src/main/java/com/wbt/fullstack/talentapigetway/TalentApiGetwayApplication.java
@@ -2,9 +2,10 @@ package com.wbt.fullstack.talentapigetway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-
+@EnableDiscoveryClient
 public class TalentApiGetwayApplication {
 
 	public static void main(String[] args) {
