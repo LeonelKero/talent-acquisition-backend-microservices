eda8c3c4c27619dee77684044cdd5a1d2890de5f
leonel ka <waboleonel@gmail.com>
2024-03-11 09:57:12 +0100

    Create api core module

---
+++ talent-core-api/src/main/java/com/wbt/talentcoreapi/TalentCoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.wbt.talentcoreapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalentCoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalentCoreApiApplication.class, args);
+	}
+
+}
---
+++ talent-core-api/src/test/java/com/wbt/talentcoreapi/TalentCoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wbt.talentcoreapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalentCoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
